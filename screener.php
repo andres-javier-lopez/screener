e49e6638738cba4df7fd8c5b7fe4c18b3495b7af
Andrés Javier López <ajavier.lopez@gmail.com>
2013-03-05 15:34:07 -0600

    agregada función de recorte

--- screener.php
+++ screener.php
@@ -1,13 +1,27 @@
 <?php
 
 if(isset($_GET['url'])) {
+	if(!file_exists('images')) {
+		mkdir('images');
+	}
+	
 	$url = 'http://'.$_GET['url'];
-	$file = $_GET['url'].'.png';
+	$file = 'images/'.$_GET['url'].'.png';
 	system('./phantomjs rasterize.js '.$url.' '.$file);
 	
-	if(file_exists($file)) {
+	list($width, $height) = getimagesize($file);
+	$new_width = $width;
+	$new_height = isset($_GET['alto'])?$_GET['alto']:$height;
+	$imagen_orig = imagecreatefrompng($file);
+	$imagen = imagecreatetruecolor($new_width, $new_height);
+	
+	imagecopyresampled($imagen, $imagen_orig, 0, 0, 0, 0, $new_width, $new_height, $width, $new_height);
+	$new_file = str_replace('.png', '.crop.png', $file);
+	imagepng($imagen, $new_file, 7);
+	
+	if(file_exists($new_file)) {
 		header('Content-Type: image/png');
-		readfile($file);
+		readfile($new_file);
 	}
 	else {
 		echo 'No se pudo crear el screenshot';
